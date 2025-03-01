b2ff9ccfbad677baa9bc83c85959ef64da2b0cb7
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-03-01 18:30:32 +0500

    AI React native expo app

---
+++ AI-Couching/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { ColoUrs } from "./../Constants/ColoUrs";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: ColoUrs.WHITE,
+      }}
+    >
+     
+    </View>
+  );
+}
