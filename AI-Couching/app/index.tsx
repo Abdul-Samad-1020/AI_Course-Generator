487fffa6419afc9aa42fd13aa54d4c6e6b677601
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-03-02 05:01:42 +0500

    Update index.tsx

--- AI-Couching/app/index.tsx
+++ AI-Couching/app/index.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
-import { ColoUrs } from "./../Constants/ColoUrs";
+
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: ColoUrs.WHITE,
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
      
